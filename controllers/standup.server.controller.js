aeeb4c4090a02bfde06c473921b9e5ec61d41735
kvin1087 <kaustubh87@gmail.com>
2016-06-13 21:59:19 -0500

    Added database code

--- controllers/standup.server.controller.js
+++ controllers/standup.server.controller.js
@@ -1,5 +1,78 @@
 var Standup = require('../models/standup.server.model.js');
 
+exports.list = function(req,res){
+	var query = Standup.find();
+	query.sort({createdOn: 'desc'})
+	.limit(12)
+	.exec(function(err, results){
+
+		res.render('index',{ title:'Standup - List', notes:results });
+
+	});
+};
+
+exports.filterByMember = function(req,res){
+
+	var query = Standup.find();
+	var filter = req.body.memberName;
+
+	query.sort({createdOn:'desc'});
+
+	if(filter.length > 0)
+	{
+		query.where({ memberName: filter });
+	}
+
+	query.exec(function(err, results){
+		res.render('index',{title: 'Standup - List' , notes: results });
+
+	});
+
+
+};
+
+/*
+var query = Standup.find(); //returns all of the documents.
+
+//callback function
+
+Standup.find(function(err, results))
+{
+
+});
+
+Standup.find({memberName:'David'} , 'memberName impediment', function(err, results){
+
+
+
+});
+
+//findOne method
+
+
+var query = Standup.findOne();
+query.exec(function(err, results){
+
+});
+
+//with conditions 
+
+var query = Standup.findOne({memberName:'David'});
+
+
+// findById
+
+var query = Standup.findById(id);
+
+Standup.findById(id).exec(function(err, results){
+
+});
+
+//$gt, $gte, $in , $lt, $lte , $ne $nin -> comparision operators
+
+
+*/
+
 exports.create = function(req,res){
 
 	var entry = new Standup ({
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,11 @@ var standupCtrl = require('../controllers/standup.server.controller.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  return standupCtrl.list(req,res);
+});
+
+router.post('/' , function(req,res){
+	return standupCtrl.filterByMember(req,res);
 });
 
 router.get('/newnote', function(req,res){
@@ -16,5 +20,6 @@ router.post('/newnote', function(req,res){
 	return standupCtrl.create(req, res);
 
 });
+
 
 module.exports = router;
